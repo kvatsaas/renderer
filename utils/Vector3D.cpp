13afd6bab242b8bd7022645a9420142cd1a1b076
Kristian Vatsaas <vatsa008@d.umn.edu>
2022-01-22 13:55:57 -0600

    Implemented Vector3D

---
+++ utils/Vector3D.cpp
@@ -0,0 +1,146 @@
+#include "Vector3D.h"
+#include <stdexcept>
+
+namespace utils {
+
+Vector3D::Vector3D()
+  : x(0), y(x), z(x) {}
+
+Vector3D::Vector3D(float a, float b, float c)
+  : x(a), y(b), z(c) {}
+
+Vector3D::Vector3D(const Vector3D &v)
+  : x(v.x), y(v.y), z(v.z) {}
+
+Vector3D Vector3D::scale(const float scalar) const
+{
+  return Vector3D(x * scalar, y * scalar, z * scalar);
+}
+
+const float Vector3D::operator[](const int index) const{
+  switch (index) {
+  case 0:
+    return x;
+  case 1:
+    return y;
+  case 2:
+    return z;
+  default:
+    throw std::out_of_range("Vector3D index range is 0-2");
+  }
+}
+
+float &Vector3D::operator[](const int index) {
+  switch (index) {
+  case 0:
+    return x;
+  case 1:
+    return y;
+  case 2:
+    return z;
+  default:
+    throw std::out_of_range("Vector3D index range is 0-2");
+  }
+}
+
+const Vector3D &Vector3D::operator=(const Vector3D &rhs)
+{
+  x = rhs.x;
+  y = rhs.y;
+  z = rhs.z;
+  return *this;
+}
+
+const Vector3D &Vector3D::operator+=(const Vector3D &rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  z += rhs.z;
+  return *this;
+}
+
+const Vector3D &Vector3D::operator-=(const Vector3D &rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  z -= rhs.z;
+  return *this;
+}
+
+const Vector3D &Vector3D::operator*=(const Vector3D &rhs)
+{
+  x *= rhs.x;
+  y *= rhs.y;
+  z *= rhs.z;
+  return *this;
+}
+
+const Vector3D &Vector3D::operator/=(const Vector3D &rhs)
+{
+  x /= rhs.x;
+  y /= rhs.y;
+  z /= rhs.z;
+  return *this;
+}
+
+const Vector3D &Vector3D::operator*=(const float &rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  z *= rhs;
+  return *this;
+}
+
+float Vector3D::length() const
+{
+  return sqrt(x * x + y * y + z * z);
+}
+
+const float Vector3D::dotProduct(const Vector3D &rhs) const
+{
+  return x * rhs.x + y * rhs.y + z * rhs.z;
+}
+
+Vector3D Vector3D::unitVector() const
+{
+  float length = this->length();
+
+  return Vector3D(x / length, y / length, z / length);
+}
+
+const Vector3D operator+(const Vector3D &lhs, const Vector3D &rhs)
+{
+  return Vector3D(lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2]);
+}
+
+const Vector3D operator-(const Vector3D &lhs, const Vector3D &rhs)
+{
+  return Vector3D(lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2]);
+}
+
+const Vector3D operator*(const float lhs, const Vector3D &rhs)
+{
+  return rhs.scale(lhs);
+}
+
+const Vector3D operator*(const Vector3D &lhs, const float rhs)
+{
+  return lhs.scale(rhs);
+}
+
+const Vector3D operator*(const Vector3D &lhs, const Vector3D &rhs)
+{
+  return Vector3D(lhs[0] * rhs[0], lhs[1] * rhs[1], lhs[2] * rhs[2]);
+}
+
+const Vector3D operator/(const Vector3D &lhs, const Vector3D &rhs)
+{
+  return Vector3D(lhs[0] / rhs[0], lhs[1] / rhs[1], lhs[2] / rhs[2]);
+}
+
+bool operator==(const Vector3D &lhs, const Vector3D &rhs)
+{
+  return (lhs[0] == rhs[0] && lhs[1] == rhs[1] && lhs[2] == rhs[2]);
+}
+
+}// namespace utils
---
+++ utils/Vector3D.h
@@ -0,0 +1,82 @@
+#ifndef _VECTOR_3D
+#define _VECTOR_3D
+
+namespace utils {
+
+class Vector3D
+{
+public:
+	// default constructor
+	Vector3D();
+
+	// basic constructor
+	Vector3D(float a, float b, float c);
+
+	// copy constructor
+	Vector3D(const Vector3D &v);
+
+	// scale vector by scalar value
+	Vector3D scale(const float scalar) const;
+
+	// access with []
+	const float operator[](const int index) const;
+
+	// change values with []
+	float &operator[](const int index);
+
+	// override = operator
+	const Vector3D &operator=(const Vector3D &rhs);
+
+    // override += operator to add two vectors
+    const Vector3D &operator+=(const Vector3D &rhs);
+
+    // override -= operator to subtract one vector from another
+    const Vector3D &operator-=(const Vector3D &rhs);
+
+    // override *= operator to multiply two vectors, value-wise
+    const Vector3D &operator*=(const Vector3D &rhs);
+
+    // override /= operator to divide one vector by another, value-wise
+    const Vector3D &operator/=(const Vector3D &rhs);
+
+	// override *= operator for scaling
+	const Vector3D &operator*=(const float &rhs);
+
+	// calculate length of a vector
+	float length() const;
+
+	// calculate dot product of two vectors
+	const float dotProduct(const Vector3D &rhs) const;
+
+	// create unit vector of this vector
+	Vector3D unitVector() const;
+
+private:
+	float x;
+	float y;
+	float z;
+};
+
+// override + operator to add two vectors
+const Vector3D operator+(const Vector3D &lhs, const Vector3D &rhs);
+
+// override - operator to subtract one vector from another
+const Vector3D operator-(const Vector3D &lhs, const Vector3D &rhs);
+
+// override * operator to scale vector, allowing for interchangeable positioning
+const Vector3D operator*(const float lhs, const Vector3D &rhs);
+const Vector3D operator*(const Vector3D &lhs, const float rhs);
+
+// override * operator to multiply two vectors, value-wise
+const Vector3D operator*(const Vector3D &lhs, const Vector3D &rhs);
+
+// override / operator to divide one vector by another, value-wise
+const Vector3D operator/(const Vector3D &lhs, const Vector3D &rhs);
+
+// test equality of two vectors
+bool operator==(const Vector3D &lhs, const Vector3D &rhs);
+
+}
+
+
+#endif
