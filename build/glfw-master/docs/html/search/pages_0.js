46db681f5e40c9386458448bd37e24405c7e01bf
Kristian Vatsaas <vatsa008@d.umn.edu>
2022-01-31 16:24:02 -0600

    Removed build folder

--- build/glfw-master/docs/html/search/pages_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['bug_20list_0',['Bug List',['../bug.html',1,'']]],
-  ['building_20applications_1',['Building applications',['../build_guide.html',1,'']]]
-];
--- build/renderer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: S:/Documents/School/UMD/CS 4212/renderer/renderer
-# Build directory: S:/Documents/School/UMD/CS 4212/renderer/build/renderer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
