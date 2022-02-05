95b488c19be9ee1c0e8ca09914e53bea125b2ab9
Kristian Vatsaas <vatsa008@d.umn.edu>
2022-02-05 11:36:22 -0600

    Quick fix for a forgotten include.

--- renderer/Shape.h
+++ renderer/Shape.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "Vector3D.h"
+#include "Ray.h"
 
 namespace renderer {
 
@@ -11,7 +12,7 @@ public:
   virtual ~Shape() = 0;
 
   virtual std::string getName() = 0;
-
+  
   virtual bool closestHit(const Ray &r) = 0; //will need to add other parameters; return true if ray intersects shape
 };
 }// namespace renderer
