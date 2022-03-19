d73a4788cc7c6d03557b907ed9b42ed152ee6c35
Kristian Vatsaas <vatsa008@d.umn.edu>
2022-03-19 17:18:25 -0500

    OpenGL sandboxing

--- OpenGL/glfwExample.cpp
+++ OpenGL/glfwExample.cpp
@@ -1,4 +1,4 @@
-#include <cstdlib>
+﻿#include <cstdlib>
 #include <iostream>
 #include <vector>
 
@@ -12,11 +12,19 @@
 
 #include "GLSL.h"
 
+double FPS = 0.0;
+
 int CheckGLErrors(const char *s)
 {
     int errCount = 0;
     
     return errCount;
+}
+
+void tPressPrintFPS(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_T && action == GLFW_PRESS)
+    std::cout << "FPS: " << FPS << std::endl;
 }
 
 int main(void)
@@ -34,8 +42,8 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    int winWidth = 1000;
-    float aspectRatio = 1.0; // 16.0 / 9.0; // winWidth / (float)winHeight;
+    int winWidth = 800;
+    float aspectRatio = 16.0 / 9.0; // winWidth / (float)winHeight;
     int winHeight = winWidth / aspectRatio;
     
     GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "OpenGL Example", NULL, NULL);
@@ -56,9 +64,32 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *version = glGetString(GL_VERSION);
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *shading = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    std::cout << "Renderer: " << renderer << std::endl;
+    std::cout << "OpenGL version supported: " << version << std::endl;
+    std::cout << "Vendor: " << vendor << std::endl;
+    std::cout << "Shading language version: " << shading << std::endl;
+
+    GLint major_version;
+    GLint minor_version;
+    GLint max_texture;
+    GLint depth_test;
+    glGetIntegerv(GL_MAJOR_VERSION, &major_version);
+    glGetIntegerv(GL_MINOR_VERSION, &minor_version);
+    glGetIntegerv(GL_MAX_TEXTURE_SIZE, &max_texture);
+    glGetIntegerv(GL_DEPTH_TEST, &depth_test);
+    std::cout << "OpenGL Major Version: " << major_version << std::endl;
+    std::cout << "OpenGL Minor Version: " << minor_version << std::endl;
+    std::cout << "OpenGL Max Texture Size: " << max_texture << std::endl;
+    std::cout << "OpenGL Depth Test Enabled: " << depth_test << std::endl;
+
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    glClearColor(0.0, 0.7, 1.0, 1.0);
+    glClearColor(0.310, 0.149, 0.514, 1.0);
+    glfwSetKeyCallback(window, tPressPrintFPS);
 
     int fb_width, fb_height;
     glfwGetFramebufferSize(window, &fb_width, &fb_height);
@@ -73,6 +104,8 @@ int main(void)
 
         endFrameTime = glfwGetTime();
         timeDiff = endFrameTime - startFrameTime;
+        FPS = 1.0 / timeDiff;
+        //std::cout << "FPS: " << FPS << std::endl;
         startFrameTime = glfwGetTime();
 
         // Clear the window's buffer (or clear the screen to our
@@ -80,7 +113,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Swap the front and back buffers
-        // glfwSwapInterval(0);
+        glfwSwapInterval(0);
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
@@ -89,7 +122,7 @@ int main(void)
         if (glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, 1);
     }
-  
+
     glfwTerminate();
     return 0;
 }
